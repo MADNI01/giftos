99aa55b895988e3e0488a8c4d3f4d6c0520204a8
MADNI01 <majdkot0@gmail.com>
2021-08-17 02:35:33 +0300

    s

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,37 @@
 import "../styles/globals.css";
 import store from "../components/store";
 import { Provider } from "react-redux/";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../firebase/firebase";
-import Loading from '../components/Loading'
+import NProgress from "nprogress";
+import Head from "next/head";
+import router from 'next/router'
+
+  
 
 function MyApp({ Component, pageProps }) {
-  const [user,loading] =useAuthState(auth)
 
-
+  router.events.on("routeChangeStart", (url) => {
+    NProgress.start();
+  });
+  router.events.on("routeChangeComplete", (url) => {
+    NProgress.done();
+  });
 
 
   return (
+    <>
+    <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
+          crossorigin="anonymous"
+          referrerpolicy="no-referrer"
+        />
+      </Head>
       <Provider store={store}>
-
       <Component {...pageProps} />
       </Provider>
-    
+    </>
   );
 }
 
